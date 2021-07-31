3de2850eb3fe1fc96ea54e221e7984167eded7b4
Atakan305 <76012121+Atakan305@users.noreply.github.com>
2021-07-31 21:27:42 +0300

    Update SpeedTest.py

--- SpeedTest.py
+++ SpeedTest.py
@@ -1,4 +1,20 @@
 import speedtest
+import time
+from imutils.video import FPS
+import argparse
+import imutils
+
+static_back = None
+motion_list = [None, None]
+time = []
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-n", "--num-frames", type=int, default=100,
+	            help="# of frames to loop over for FPS test")
+ap.add_argument("-d", "--display", type=int, default=-1,
+	            help="Whether or not frames should be displayed")
+args = vars(ap.parse_args())
+fps = FPS().start()
 
 s = speedtest.Speedtest()
 
@@ -12,3 +28,6 @@ print(f"Download Speed: {downloadSpeed:.2f} Mbps")
 print(f"Upload Speed: {uploadSpeed:.2f} Mbps")
 print(f"Ping: {pingResult} ms")
 
+fps.update()
+fps.stop()
+print("[INFO] elapsed time: {:.2f}".format(fps.elapsed()))
